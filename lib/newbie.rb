98228f2036d24cfe623e2c024ac0c1de7c060f77
kpeck71 <kpeck71@gmail.com>
2017-11-06 23:03:34 -0500

    setting up folder structure and base files

---
+++ lib/newbie.rb
@@ -0,0 +1,13 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative "./newbie/version"
+require_relative "./newbie/cli"
+require_relative "./newbie/scraper"
+require_relative "./newbie/episode"
+
+
+module Newbie
+  # Your code goes here...
+end
