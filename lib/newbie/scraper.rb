4e1029d141d22e1a51d4d213e7e1a96378be3945
kpeck71 <kpeck71@gmail.com>
2017-11-07 21:56:12 -0500

    add scrape_episode
    add scrape_episodes so an Episode object's properties can be scraped from the website

--- lib/newbie/scraper.rb
+++ lib/newbie/scraper.rb
@@ -1,2 +1,13 @@
 class Newbie::Scraper
+  def scrape_episodes
+    @doc = Nokogiri::HTML(open("https://www.codenewbie.org/podcast"))
+    @doc.search("article").each do |eps|
+      episode = TestingCli::Episode.new
+      episode.title = eps.search("a h3").text
+      episode.short_des = eps.search(".small-12.medium-8.large-12.columns p").text
+      episode.url = eps.search("a").attr("href").value
+      episode.date = eps.search(".subtitle").text
+      episode.save
+    end
+  end
 end
