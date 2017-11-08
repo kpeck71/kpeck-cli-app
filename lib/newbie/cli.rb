9cf638eaf1611715230a47a19e70833a9a268d36
kpeck71 <kpeck71@gmail.com>
2017-11-07 22:04:48 -0500

    def call method and testing other methods in cli

--- lib/newbie/cli.rb
+++ lib/newbie/cli.rb
@@ -1,2 +1,18 @@
 class Newbie::CLI
+  def call
+    puts "Testing 1-2-3"
+    Newbie::Scraper.new.scrape_episodes
+    print_episodes
+    options
+  end
+
+  def print_episodes
+    Newbie::Episode.all.each do |ep|
+      puts ep.title
+    end
+  end
+  
+  def options
+    puts "option test"
+  end
 end
