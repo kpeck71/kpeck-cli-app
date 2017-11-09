2e02a7293326c2e200b056edc3312e806a8cf47d
kpeck71 <kpeck71@gmail.com>
2017-11-09 16:12:06 -0500

    more changes to cli options.
    exit not quite working on all entries in options.

--- lib/newbie/cli.rb
+++ lib/newbie/cli.rb
@@ -12,42 +12,48 @@ class Newbie::CLI
     end
   end
   
-  def options
+ def options
     input = nil
     while input != "exit"
+      puts " "
       puts "Which episode would you like to learn more about? Please enter the episode number:"
-      number = gets.strip
-      if number.to_i <= TestingCli::Episode.all.size
-        episode = TestingCli::Episode.all[-(number.to_i)]
+      num_episodes = TestingCli::Episode.all.size
+      answer = gets.strip
+      if answer.to_i <= num_episodes && answer.to_i != 0
+        episode = TestingCli::Episode.all[-(answer.to_i)] #make sure 0 is not a valid entry
         puts "#{episode.title}"
         puts "#{episode.short_des}"
-        puts "\nIf you would like to learn more about this episode, enter 'more'. To see full list, enter 'list', or type 'exit'."
-      else
-        puts "This is not a valid episode number."
+        puts "\nIf you would like to learn more about this episode, enter 'more'. To see full list of episodes again, enter 'list', or type 'exit'."
+        puts " "
+      elsif 0 || answer > num_episodes || answer !="exit"
+        puts "This is not a valid entry. Please enter restart to try again."
+      elsif answer == "exit"
+        exit
       end
-        answer = gets.strip
-        case answer
-        when "more"
+      #else "not a valid entry. How to get it to restart and accept other episode numbers?"
+      input = gets.strip
+      case input
+      when "more"
           episode.content
           puts "\nWould you like to see the full episode list again? Enter 'list'."
           puts "Or enter 'exit'."
             answer2 = gets.strip #downcase?
-            case answer2
-            when "list"
+            if answer2 == "list"
               print_episodes
               puts "Please enter an episode number."
-            when "exit"
+            else answer2 == "exit"
               exit
             end
-        when "list"
+      when "list"
           print_episodes
           puts "Please enter an episode number."
-        when "exit"
-          exit
-        else
-          "That doesn't look like a valid entry. Please enter an episode number, 'list', or 'exit'."
-        end
+      when "exit"
+        exit
+      else
+          "That doesn't look like a valid entry. Please enter a valid episode number, 'list', or 'exit'."
       end
     end
   end
+
+end
 end
