47cf1d2a7ece7bf2a102880ee622d987c39d0f74
kpeck71 <kpeck71@gmail.com>
2017-11-09 15:06:30 -0500

    updating case statements and fixed missing gets.strip

--- lib/newbie/cli.rb
+++ lib/newbie/cli.rb
@@ -22,16 +22,28 @@ class Newbie::CLI
         puts "#{episode.title}"
         puts "#{episode.short_des}"
         puts "\nIf you would like to learn more about this episode, enter 'more'. To see full list, enter 'list', or type 'exit'."
-      answer = gets.strip
+      else
+        puts "This is not a valid episode number."
+      end
+        answer = gets.strip
         case answer
         when "more"
           episode.content
           puts "\nWould you like to see the full episode list again? Enter 'list'."
           puts "Or enter 'exit'."
+            answer2 = gets.strip #downcase?
+            case answer2
+            when "list"
+              print_episodes
+              puts "Please enter an episode number."
+            when "exit"
+              exit
+            end
         when "list"
           print_episodes
+          puts "Please enter an episode number."
         when "exit"
-          break
+          exit
         else
           "That doesn't look like a valid entry. Please enter an episode number, 'list', or 'exit'."
         end
