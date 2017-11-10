23149f6a41dfe7cd22ddfccf3a0ba72156ebb71f
Kristen Peck <kpeck71@gmail.com>
2017-11-10 00:32:21 +0000

    update scrap_details with more dummy text

--- README.md
+++ README.md
@@ -30,4 +30,4 @@ To install this gem onto your local machine, run `bundle exec rake install`. To 
 
 ## Contributing
 
-Bug reports and pull requests are welcome on GitHub at https://github.com/'kpeck71'/testing_cli.
+Bug reports and pull requests are welcome on GitHub at https://github.com/'kpeck71'/kpeck-cli-app.
--- lib/newbie/scraper.rb
+++ lib/newbie/scraper.rb
@@ -14,9 +14,14 @@ class Newbie::Scraper
   def scrape_details
     @doc = Nokogiri::HTML(open("https://www.codenewbie.org#{@url}"))
     puts "--------------- Episode Description ---------------"
+    #@doc.search(".medium-8.large-8.columns.card p").text
     puts "Here is a fake description until I figure out how to scrape pages that have different layouts."
     puts "--------------- About the Guest ---------------"
     puts "Guest Name"
+    puts "Guest detail paragraph"
+    #@doc.search(".columns.medium-4.large-4 h4").text.strip
+    puts "Contact Details:"
+    #@doc.search("a.social-link").attr("href").value
     puts "Here is another part of the episode page I could add with the guest paragraph."
   end
 
