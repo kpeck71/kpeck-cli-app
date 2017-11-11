b2b82e7c2e5114b9bce7b1613d597b1ca4a48b00
Kristen Peck <kpeck71@gmail.com>
2017-11-11 14:24:03 +0000

    cli updates - removing excess

--- lib/newbie/cli.rb
+++ lib/newbie/cli.rb
@@ -13,29 +13,32 @@ class Newbie::CLI
 
  def options
     input = nil
+    num_episodes = Newbie::Episode.all.size
     while input != "exit"
       puts "\nWhich set of episodes would like to like to see? Enter 1-50, 51-100, 101-150, or 151 and up."
-      input = gets.strip.to_i #this changes 1-50 to just the integer 1
-
-      list_episode_range(input)
-      puts "\nWhich episode would you like to learn more about? Please enter the episode number:"
-      num_episodes = Newbie::Episode.all.size
-      answer = gets.strip
-      if answer.to_i <= num_episodes && answer.to_i != 0
-        episode = Newbie::Episode.all[-(answer.to_i)] #make sure 0 is not a valid entry
-        puts "#{episode.title}"
-        puts "#{episode.short_des}"
-        puts "\nIf you would like to learn more about this episode, enter 'more' or type 'exit'."
-        puts " "
-      else 0 || answer > num_episodes
-        puts "This is not a valid entry. Please enter restart to try again."
-      end
-      input = gets.strip
-      case input
-      when "more"
+      puts "For a full list of all #{num_episodes} episodes, enter 'list'."
+      input = gets.strip #this changes a string like "1-50" to the integer 1
+      if input != "list"
+        list_episode_range(input.to_i)
+        puts "\nWhich episode would you like to learn more about? Please enter the episode number:"
+        input = gets.strip
+        if  input == "0" || input.to_i > num_episodes
+          puts "This is not a valid entry. Please enter restart to try again."
+        else input.to_i <= num_episodes && input.to_i != 0 #make separate list_episode method?
+          episode = Newbie::Episode.all[-(input.to_i)] #make sure 0 is not a valid entry
+          puts "#{episode.title}"
+          puts "#{episode.short_des}"
+          puts "\nIf you would like to learn more about this episode, enter 'more' or type 'exit'."
+          puts " "
+        end
+        input = gets.strip.downcase
+        if input == "more"
           episode.content
-      else
-          "That doesn't look like a valid entry. Please enter a valid episode number, 'list', or 'exit'."
+          "Would you like to see more episodes? Enter 'Y' or exit."
+        end
+
+      else input.downcase == "list"
+          print_episodes
       end
     end
     puts "See you next time, newbie"
@@ -48,4 +51,8 @@ class Newbie::CLI
        puts "#{episode.title}"
      end
    end
+
+  #  def list_episode(number)
+  #
+  #  end
 end
